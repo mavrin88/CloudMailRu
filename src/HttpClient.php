286885d0cb83ef8be83d2d41dd2fdfbe4245c8bd
mavrin_88 <mavrin_88@mail.ru>
2021-06-01 16:43:56 +0300

    Update request methods and add update checkResponse method

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -17,37 +17,72 @@ class HttpClient
 
     public function __construct()
     {
-
+        $this->client = new Client();
+        $this->cookie = new CookieJar();
     }
 
 
-    public function requestGet()
+    public function requestGet(string $url, array $headers = [])
     {
-
+        try {
+            return $response = $this->client->request('GET', $url, [
+                'cookies' => $this->cookie,
+                'verify' => false,
+                'headers' => $headers,
+            ]);
+        } catch (GuzzleException $e) {
+            throw new CloudMailRuException($e->getMessage());
+        }
     }
 
 
-    public function requestPost()
+    public function requestPost(string $url, array $postParams, array $headers = [])
     {
-
+        try {
+            return $response = $this->client->request('POST', $url, [
+                'form_params' => $postParams,
+                'cookies' => $this->cookie,
+                'verify' => false,
+                'headers' => $headers,
+            ]);
+        } catch (GuzzleException $e) {
+            throw new CloudMailRuException($e->getMessage());
+        }
     }
 
 
-    public function requestPut()
+    public function requestPut(string $url, $body, array $headers = [])
     {
-
+        try {
+            return $response = $this->client->request('PUT', $url, [
+                'body' => $body,
+                'cookies' => $this->cookie,
+                'verify' => false,
+                'headers' => $headers,
+            ]);
+        } catch (GuzzleException $e) {
+            throw new CloudMailRuException($e->getMessage());
+        }
     }
 
 
     public function requestDelete()
     {
+        try {
 
+        } catch (GuzzleException $e) {
+            throw new CloudMailRuException($e->getMessage());
+        }
     }
 
 
-    public function checkResponse()
+    public function checkResponse(ResponseInterface $response)
     {
-
+        if ($response->getStatusCode() == 200) {
+            return $response->getBody()->getContents();
+        } else {
+            throw new CloudMailRuException('Bad response ' . $response->getStatusCode());
+        }
     }
 
 }
