66d536905d907b3e50cc2bee62cf9924453dbcce
mavrin_88 <mavrin_88@mail.ru>
2021-06-01 15:59:55 +0300

    Added the Http client class framework

---
+++ src/HttpClient.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AlexeyMarchenko\CloudMailRu;
+
+use GuzzleHttp\{Client, Cookie\CookieJar, Exception\GuzzleException};
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+
+
+class HttpClient
+{
+
+    public $client;
+
+    private $cookie;
+    
+
+    public function __construct()
+    {
+
+    }
+
+
+    public function requestPost()
+    {
+
+    }
+
+
+    public function requestPut()
+    {
+
+    }
+
+
+    public function requestGet()
+    {
+
+    }
+
+
+    public function checkResponse()
+    {
+
+    }
+
+}
